d1c9052778b0d747230f6a287628f1866968485f
Christian Broms <cb@christianbroms.com>
2020-05-24 18:52:15 -0700

    add new post and fix font-size issue

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5fd56093c5acd2ac7455";
+/******/ 	var hotCurrentHash = "6faacf76e95203c4f144";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
